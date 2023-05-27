13369eeac2ecc335338cc619570e8f399dd1f5c6
manuelnones <manuelnones99@yahoo.com>
2023-05-27 12:54:35 +0200

    add model + migration technologies + migration post_technology + methods many to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,8 @@ class Post extends Model
     public function type() {
        return $this->belongsTo(Type::class);
     }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
 }
